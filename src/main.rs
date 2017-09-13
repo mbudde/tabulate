f0bae1d7760d023e2904a64be1719cb0df371e0e
Michael Budde <mbu@jobindex.dk>
2017-09-13 16:26:16 +0200

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,8 @@ impl Column {
     fn calculate_size(&mut self, ratio: f64) {
         assert!(self.samples.len() > 0);
 
-        if ratio == 0. { // Optimization
+        if ratio == 0. {
+            // Optimization
             self.size = Some(self.samples.iter().map(|p| p.0).max().unwrap_or(0));
         }
 
@@ -40,15 +41,20 @@ impl Column {
         let min = self.samples.iter().map(|p| p.0).min().unwrap();
         let max = self.samples.iter().map(|p| p.0).max().unwrap();
         let spread = (0.7 + 20.0 / (1 + max - min) as f64).powi(2);
-        let prob = self.samples.iter().map(|&(s, x)| (s, x as f64 / n as f64)).collect::<Vec<_>>();
+        let prob = self.samples
+            .iter()
+            .map(|&(s, x)| (s, x as f64 / n as f64))
+            .collect::<Vec<_>>();
 
         let mut best_score = std::f64::MAX;
         let mut best_size = max;
-        for l in min .. max+1 {
-            let waste: f64 = prob.iter().take_while(|&&(s, _)| s < l)
+        for l in min..max + 1 {
+            let waste: f64 = prob.iter()
+                .take_while(|&&(s, _)| s < l)
                 .map(|&(s, p)| p * l.saturating_sub(s) as f64)
                 .sum();
-            let overflow: f64 = prob.iter().skip_while(|&&(s, _)| s <= l)
+            let overflow: f64 = prob.iter()
+                .skip_while(|&&(s, _)| s <= l)
                 .map(|&(s, p)| p * s.saturating_sub(l) as f64)
                 .sum();
 
@@ -85,31 +91,70 @@ fn main() {
 
 fn run() -> io::Result<()> {
     let matches = App::new("tabulate")
-        .arg(Arg::from_usage("-t, --truncate 'Truncate data that does not fit in a column'"))
-        .arg(Arg::from_usage("-c, --compress-cols=[RATIO] 'Compress columns so more data fits on the screen'"))
-        .arg(Arg::from_usage("-n, --estimate-count=[N] 'Estimate column sizes from the first N lines'"))
-        .arg(Arg::from_usage("-i, --include=[COLS] 'Columns to show (starts from 0, defaults to all columns)'"))
-        .arg(Arg::from_usage("-x, --exclude=[COLS] 'Columns to hide (starts from 0; defaults to no columns)'"))
+        .arg(Arg::from_usage(
+            "-t, --truncate 'Truncate data that does not fit in a column'",
+        ))
+        .arg(Arg::from_usage(
+            "-c, --compress-cols=[RATIO] 'Compress columns so more data fits on the screen'",
+        ))
+        .arg(Arg::from_usage(
+            "-n, --estimate-count=[N] 'Estimate column sizes from the first N lines'",
+        ))
+        .arg(Arg::from_usage(
+            "-i, --include=[COLS] 'Columns to show (starts from 0, defaults to all columns)'",
+        ))
+        .arg(Arg::from_usage(
+            "-x, --exclude=[COLS] 'Columns to hide (starts from 0; defaults to no columns)'",
+        ))
         .get_matches();
 
     let opt_truncate = matches.is_present("truncate");
 
-    let opt_ratio = matches.value_of("compress-cols")
-        .map(|m| m.parse().map_err(|_| "could not parse value as a floating number".to_string()))
+    let opt_ratio = matches
+        .value_of("compress-cols")
+        .map(|m| {
+            m.parse().map_err(|_| {
+                "could not parse value as a floating number".to_string()
+            })
+        })
         .unwrap_or(Ok((1.0)))
         .unwrap();
 
-    let opt_lines = matches.value_of("estimate-count")
-        .map(|m| m.parse().map_err(|_| "could not parse value as a number".to_string()))
+    let opt_lines = matches
+        .value_of("estimate-count")
+        .map(|m| {
+            m.parse().map_err(
+                |_| "could not parse value as a number".to_string(),
+            )
+        })
         .unwrap_or(Ok((1000)))
         .unwrap();
 
-    let opt_include_cols = matches.value_of("include")
-        .map(|m| m.split(',').map(|s| s.parse().map_err(|_| "could not parse value as a list of ranges".to_string())).collect::<Result<Vec<_>, _>>().map(Some))
+    let opt_include_cols = matches
+        .value_of("include")
+        .map(|m| {
+            m.split(',')
+                .map(|s| {
+                    s.parse().map_err(|_| {
+                        "could not parse value as a list of ranges".to_string()
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()
+                .map(Some)
+        })
         .unwrap_or(Ok(None))
         .unwrap();
-    let opt_exclude_cols = matches.value_of("exclude")
-        .map(|m| m.split(',').map(|s| s.parse().map_err(|_| "could not parse value as a list of ranges".to_string())).collect::<Result<Vec<_>, _>>())
+    let opt_exclude_cols = matches
+        .value_of("exclude")
+        .map(|m| {
+            m.split(',')
+                .map(|s| {
+                    s.parse().map_err(|_| {
+                        "could not parse value as a list of ranges".to_string()
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()
+        })
         .unwrap_or(Ok(vec![]))
         .unwrap();
 
@@ -125,7 +170,12 @@ fn run() -> io::Result<()> {
         let line = line.unwrap();
         split_line(&line, &mut row);
         if measuring {
-            update_columns(&mut columns, &row[..], opt_include_cols.as_ref(), &opt_exclude_cols[..]);
+            update_columns(
+                &mut columns,
+                &row[..],
+                opt_include_cols.as_ref(),
+                &opt_exclude_cols[..],
+            );
             backlog.push(row.clone());
             if backlog.len() >= opt_lines {
                 measuring = false;
@@ -204,7 +254,7 @@ fn split_line(input: &str, row: &mut Vec<String>) {
                 if ch == delim {
                     if let Some(s) = start {
                         // println!("output = {:?}", &input[s..i+1]);
-                        row.push(input[s..i+1].to_owned());
+                        row.push(input[s..i + 1].to_owned());
                     }
                     start = None;
                     state = Whitespace;
@@ -222,13 +272,21 @@ fn split_line(input: &str, row: &mut Vec<String>) {
     }
 }
 
-fn update_columns(columns: &mut Vec<Column>, row: &[String], include_cols: Option<&Vec<Range>>, excluded_cols: &[Range]) {
+fn update_columns(
+    columns: &mut Vec<Column>,
+    row: &[String],
+    include_cols: Option<&Vec<Range>>,
+    excluded_cols: &[Range],
+) {
     for i in 0..min(columns.len(), row.len()) {
         columns[i].update(row[i].len());
     }
     for i in columns.len()..row.len() {
         let mut col = Column::new(row[i].len());
-        if include_cols.map(|v| !v.iter().any(|r| r.contains(i as u32))).unwrap_or(false) {
+        if include_cols
+            .map(|v| !v.iter().any(|r| r.contains(i as u32)))
+            .unwrap_or(false)
+        {
             col.excluded = true;
         }
         if excluded_cols.iter().any(|r| r.contains(i as u32)) {
@@ -238,11 +296,19 @@ fn update_columns(columns: &mut Vec<Column>, row: &[String], include_cols: Optio
     }
 }
 
-fn print_row<W: Write>(out: &mut W, columns: &[Column], row: &[String], truncate: bool) -> io::Result<()> {
+fn print_row<W: Write>(
+    out: &mut W,
+    columns: &[Column],
+    row: &[String],
+    truncate: bool,
+) -> io::Result<()> {
     let mut first = true;
     let mut goal: usize = 0;
     let mut used: usize = 0;
-    for (cell, col) in row.iter().zip(columns.iter()).filter(|&(_, col)| !col.excluded) {
+    for (cell, col) in row.iter().zip(columns.iter()).filter(
+        |&(_, col)| !col.excluded,
+    )
+    {
         if !first {
             write!(out, "  ")?;
         }
@@ -250,7 +316,7 @@ fn print_row<W: Write>(out: &mut W, columns: &[Column], row: &[String], truncate
         let width = col.size();
         let out_width = width.saturating_sub(used.saturating_sub(goal));
         if truncate && cell.len() > out_width {
-            write!(out, "{}…", &cell[0..out_width-1])?;
+            write!(out, "{}…", &cell[0..out_width - 1])?;
             used += out_width;
         } else {
             write!(out, "{:1$}", cell, out_width)?;
@@ -273,8 +339,8 @@ impl Range {
     fn contains(&self, n: u32) -> bool {
         use Range::*;
         match *self {
-            From(a)       => a <= n,
-            To(b)         =>           n <= b,
+            From(a) => a <= n,
+            To(b) => n <= b,
             Between(a, b) => a <= n && n <= b,
         }
     }
@@ -284,18 +350,21 @@ impl FromStr for Range {
     type Err = String;
     fn from_str(s: &str) -> Result<Range, String> {
         use Range::*;
-        let num = || many1(digit())
-            .map(|string: String| string.parse::<u32>().unwrap());
+        let num = || many1(digit()).map(|string: String| string.parse::<u32>().unwrap());
 
-        let mut range = num().and(optional(token('-').with(optional(num()))))
+        let mut range = num()
+            .and(optional(token('-').with(optional(num()))))
             .map(|(a, b)| match b {
                 Some(Some(b)) => Between(a, b),
                 Some(None) => From(a),
-                None    => Between(a, a),
+                None => Between(a, a),
             })
             .or(token('-').with(num()).map(|b| To(b)))
             .skip(eof());
 
-        range.parse(s).map_err(|_| "could not parse range".to_string()).map(|o| o.0)
+        range
+            .parse(s)
+            .map_err(|_| "could not parse range".to_string())
+            .map(|o| o.0)
     }
 }
