15bbc5acfd2438586fd0d7e40dd80ebd0a01b54c
Michael Budde <mbudde@gmail.com>
2018-05-26 15:23:40 +0200

    Add new fuzz target

---
+++ fuzz/fuzz_targets/truncate.rs
@@ -0,0 +1,29 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate tabulate;
+
+use std::io::BufReader;
+
+use tabulate::{
+    Options,
+    range::Ranges,
+};
+
+fuzz_target!(|data: &[u8]| {
+    let opts = Options {
+        truncate: Some(Ranges::new()),
+        ratio: 1.0,
+        lines: 1000,
+        include_cols: None,
+        exclude_cols: Ranges::new(),
+        delim: " \t".to_string(),
+        strict_delim: false,
+        print_info: false,
+        online: false,
+    };
+
+    let reader = BufReader::new(data);
+    let output: Vec<u8> = Vec::new();
+
+    tabulate::process(reader, output, &opts).ok();
+});
--- seasoned-software.sh
+++ seasoned-software.sh
@@ -3,20 +3,16 @@
 export CFLAGS=
 export CXXFLAGS=
 
-# Cargo-fuzz needs nightly rust, so switch this project to nightly
-rustup override set nightly
-
 # Cleanout any old build artifacts
 (cd fuzz && cargo clean)
 
-# Name of the fuzz target
-FUZZ_TARGET=fuzz_target_1
+for FUZZ_TARGET in defaults truncate; do
+    # Build the fuzz target in release mode by asking it to run it for 0 iterations
+    cargo +nightly fuzz run $FUZZ_TARGET --release -- -runs=0
 
-# Build the fuzz target in release mode by asking it to run it for 0 iterations
-cargo fuzz run $FUZZ_TARGET --release -- -runs=0
+    # Find the executable for the fuzz target
+    EXE="$(find fuzz/target -name $FUZZ_TARGET -executable)"
 
-# Find the executable for the fuzz target
-EXE="$(find fuzz/target -name $FUZZ_TARGET -executable)"
-
-# Upload the executable for the fuzz target
-upload-binary "$EXE"
+    # Upload the executable for the fuzz target
+    upload-binary "$EXE"
+done
